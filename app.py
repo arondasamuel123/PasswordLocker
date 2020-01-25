b9b3daf5dc05f59751b186a4cae010d811ee5bd4
arondasamuel123 <arondasamuel123@gmail.com>
2020-01-25 17:26:33 +0300

    Login functionality has been added to the app.py file. User can now Login

--- app.py
+++ app.py
@@ -18,35 +18,51 @@ def find_client(password):
     Function to login in user to the application
     """
     return Client.find_client(password)
+def create_acc(acc_username, platform_name, acc_password):
+    new_account = AccountDetails(acc_username,platform_name,acc_password)
 
 
 def main():
     while True:
-    print("Hello, I am your personal Password Vault. What would like to do?")
-    print("1.Register   2.Login")
-    option = int(input())
+        print("Hello, This will be your personal Password Vault. What would like to do?")
+        print("1.Register   2.Login")
+        option = int(input())
 
-    if option == 1:
-        print("Alright lets get started!!")
-        print("Enter your first name")
-        print("."*20)
-        first_n = input()
+        if option == 1:
+            print("Alright lets get started!!")
+            print("Enter your first name")
+            print("."*20)
+            first_n = input()
 
-        print("Enter your last name")
-        print("."*20)
-        last_n = input()
+            print("Enter your last name")
+            print("."*20)
+            last_n = input()
 
-        print("Enter your email")
-        print("."*20)
-        email = input()
+            print("Enter your email")
+            print("."*20)
+            email = input()
 
-        print("Enter your password you like to use")
-        print("."*20)
-        password = input()
+            print("Enter your password you like to use")
+            print("."*20)
+            password = input()
 
-        save_client(create_client(first_n, last_n, email, password))
+            save_client(create_client(first_n, last_n, email, password))
 
-        print(f"Thank you for signing up {first_n} {last_n}. You can now Login")
+            print(f"Thank you for signing up {first_n} {last_n}. You can now Login")
+            
+
+        elif option == 2:
+            print("Enter Email")
+            accessemail = input()
+            print("Enter Password")
+            accesspass = input()
+            if find_client(accesspass):
+                print(f"Welcome to your Password Vault {accessemail}. What would like to do?")
+                print("1.Add credentials 2. View credentials")
+            else:
+                print("Invalid credentials")
+
+        
 
 if __name__ == '__main__':
 
