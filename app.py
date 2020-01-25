04eeed69d4c257638ed93e155ac3d6d4e7f814e0
arondasamuel123 <arondasamuel123@gmail.com>
2020-01-25 15:54:44 +0300

    Create the functions for the run file to create a user account, save user to a list, login

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
 from client import Client
 from account_details import AccountDetails
 
+def create_client(fn, ln, email, password):
+    """
+    Function to create user
+    """
+    new_acc = Client(fn,ln, email, password)
+    return new_acc
+def save_client(client):
+    """
+    Function to save the user to a list
+    """
+    client.save_client()
+
+def find_client(password)
+    """
+    Function to login in user to the application
+    """
+    return Client.find_client(password)
 
 
 main()
